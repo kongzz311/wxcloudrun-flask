6637c621775dd48b38e0ef41864a79a1d6df54b5
KK <kozenzei@outlook.com>
2022-05-26 01:14:51 +0800

    init

---
+++ wxcloudrun/mapper.py
@@ -0,0 +1,47 @@
+
+def get_database(db_name):
+    from pymongo import MongoClient
+    import pymongo
+    import os
+    user = os.environ['user']
+    pwd = os.environ['pwd']
+    ip = os.environ['ip']
+
+    # Provide the mongodb atlas url to connect python to mongodb using pymongo
+    CONNECTION_STRING = f"mongodb://{user}:{pwd}@{ip}:27017/"
+
+    # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
+    from pymongo import MongoClient
+    client = MongoClient(CONNECTION_STRING)
+
+    # Create the database for our example (we will use the same database throughout the tutorial
+    return client[db_name]
+
+def addUser(qu,jie,id):
+    db = get_database('sam')
+    collection = db['users']
+    data = collection.find_one({'id': id})
+    if data is None:
+        data = {}
+        data['id'] = id
+    data['qu'] = qu
+    data['jie'] = jie        
+    x = collection.find_one_and_update(
+        {'id': id},
+        {'$set': data,
+            #  "$currentDate": {"lastModified": True}
+            },
+        upsert=True
+    )
+
+def getUser(id):
+    db = get_database('sam')
+    collection = db['users']
+    data = collection.find_one({'id': id})
+    return data
+
+def delUser(id):
+    db = get_database('sam')
+    collection = db['users']
+    data = collection.delete_one({'id':id})
+    return data
